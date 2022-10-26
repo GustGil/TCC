31b4c7426b429995861079f902c7c93f017a4d0f
GustGil <ggil04833@gmail.com>
2022-10-26 11:09:30 -0300

    1.0.0

---
+++ tela_servicos/servicos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contabilidade Gil</title>
+    <link rel="stylesheet" href="style4.css" />
+</head>
+<body>
+    <div class="main">
+        <div class="header">
+            <div class="logo">
+                <a href="/TCC/pagina_init/index.php"><img src="" alt=""></a>
+            </div>
+            <div class="menu">
+                <div>
+                    <a href="/TCC/tela_servicos/servicos.php">Serviços</a>
+                </div>
+                <div>
+                        <a href="">Sobre nós</a>
+                </div>
+                <div>
+                        <a href="/TCC/pagina_login/telaLogin.php" class="botao_login" style="text-decoration: none">Entrar</a> 
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
