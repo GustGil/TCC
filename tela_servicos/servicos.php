663e2cfee947af8dfb924348914f4a13b6de1e3f
GustGil <ggil04833@gmail.com>
2022-10-28 12:25:20 -0300

    1.0.0

--- tela_servicos/servicos.php
+++ tela_servicos/servicos.php
@@ -25,7 +25,40 @@
                 </div>
             </div>
         </div>
+        <div class="titulo">
+            <h1> Serviços</h1>
+            <div class="linha"></div>
+        </div>
+        <div class="cards">
+            <div class="cards-int">
+                <div class="IR-card">
+                    <div class="img">
+                        <img src="/TCC/tela_servicos/assets/assessoria-contabil-360x225.png" alt="">
+                    </div>
+                    <div class="texto">
+                        <h2 class="titulo-card">Imposto de Renda</h2>
+                    </div>
+                </div>
+                <div class="EF-card">
+                    <div>
+                        <img src="/TCC/tela_servicos/assets/assessoria-fiscal-e-tributaria-360x225.png" alt="">
+                    </div>
+                    <div class="texto">
+                        <h2 class="titulo-card">Escrita Fiscal</h2>
+                    </div>
+                </div>
+                <div class="FP-card">
+                    <div>
+                        <img src="/TCC/tela_servicos/assets/assessoria-trabalhista-360x225.png" alt="">
+                    </div>
+                    <div class="texto">
+                        <h2 class="titulo-card">Folha de Pagamento</h2>
+                    </div>
+                </div>
 
+
+            </div>
+        </div>
     </div>
 </body>
 </html>
