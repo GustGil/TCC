85820f2d6427deca6616c0b12464ef6cf1490cdd
da-padaria <theos.a@outlook.com>
2022-11-19 16:14:07 -0300

    agora vai

--- Registro/Register.php
+++ Registro/Register.php
@@ -27,7 +27,7 @@ include('C:\xampp\htdocs\truecc\TCC\conexao.php');
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Contabilidade Gil</title>
     <link rel="stylesheet" href="style3.css">
 </head>
 <body>
